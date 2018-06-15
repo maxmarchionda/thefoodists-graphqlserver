a9cb0cafb89119a4cb9c2c6582165b5ad735a74b
QuantumArcade <maxmarchionda@gmail.com>
2018-06-15 08:04:15 -0500

    rename config var

--- src/server.js
+++ src/server.js
@@ -6,15 +6,15 @@ require("babel-core/register");
 require("babel-polyfill");
 require('dotenv').config();
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!process.env.MONGO_URL) {
+if (!process.env.MONGODB_URI) {
   throw new Error(
     "PLease provide a mongo URL for db connection in MONGO_URL."
   );
 }
 
-mongoose.connect(MONGO_URL);
+mongoose.connect(MONGODB_URI);
 mongoose.connection.on('error', function(){throw new Error('Error connecting to MongoDB')});
 
 mongoose.connection.on('connected', function(){
